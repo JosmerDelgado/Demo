5ddc908bcb03f1f9ecf45930b60a87fb87a678d6
josmer.delgado <josmer.delgado@fdvsolutions.com>
2017-07-05 22:09:29 -0300

    api2

--- src/main/java/api/apps/Speedtest/Speedtest.java
+++ src/main/java/api/apps/Speedtest/Speedtest.java
@@ -1,7 +1,35 @@
 package api.apps.Speedtest;
 
+import api.android.Android;
+import api.interfaces.Application;
+
 /**
  * Created by josmer on 7/5/17.
  */
-public class Speedtest {
+public class Speedtest implements Application {
+    @Override
+    public void forceStop() {
+        Android.adb.forceStopApp(packageID());
+    }
+
+    @Override
+    public void clearData() {
+        Android.adb.clearAppsData(packageID());
+    }
+
+    @Override
+    public Object open() {
+        Android.adb.openAppsActivity(packageID(), activityID());
+        return null;
+    }
+
+    @Override
+    public String packageID() {
+        return "org.zwanoo.android.speedtest";
+    }
+
+    @Override
+    public String activityID() {
+        return "com.ookla.speedtest.softfacade.MainActivity";
+    }
 }
